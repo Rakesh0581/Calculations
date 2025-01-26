8917c6737b415305e1c11b9692b3e4aa7f2c99af
Rakesh0581 <rakesh_adupa@srivensolutions.com>
2025-01-26 23:19:58 +0530

    removed the square

--- Calculations.py
+++ Calculations.py
@@ -6,11 +6,11 @@ Addition = num1 + num2
 subtraction = num1 - num2
 Div = num1/num2
 Mult = num1 * num2
-Square = num1 ** num2
+# Square = num1 ** num2
 
 
 print("Addition",Addition)
 print("subtraction",subtraction)
 print("Division",Div)
 print("Multiplication",Mult)
-print("Square",Square)
+# print("Square",Square)
